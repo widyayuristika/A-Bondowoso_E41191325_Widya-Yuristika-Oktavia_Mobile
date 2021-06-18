cce28c08a5df6b8621654ff22beef45b8038cd72
Widya Yuristika Oktavia <yuristikawidya@gmail.com>
2021-06-18 16:14:25 +0700

    Tugas10 LBS dan Akses Sensor

---
+++ Tugas_Minggu10_LBSdan Akses Sensor/Tugas10AksesSensor/app/src/main/java/widya/example/tugas10aksessensor/MainActivity.java
@@ -0,0 +1,50 @@
+package widya.example.tugas10aksessensor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imgAllSensor, imgProximitySensor, imgLightSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgAllSensor = findViewById(R.id.img_list_all_sensor);
+        imgLightSensor = findViewById(R.id.img_light_sensor);
+        imgProximitySensor = findViewById(R.id.img_proximity_sensor);
+        imgAllSensor.setOnClickListener(this);
+        imgLightSensor.setOnClickListener(this);
+        imgProximitySensor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+
+            case R.id.img_list_all_sensor:
+                Intent intentAll = new Intent(this, AllSensorActivity.class);
+                startActivity(intentAll);
+                break;
+
+            case R.id.img_light_sensor:
+                Intent intentLight = new Intent(this, LightSensorActivity.class);
+                startActivity(intentLight);
+                break;
+
+            case R.id.img_proximity_sensor:
+                Intent intent = new Intent(this, ProximitySensorActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+    }
+}
